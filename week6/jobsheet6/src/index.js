597fb7617d9e6a93e58c2db59f2f5fe1d0fa18dd
revindaamalias <revindaamalia@gmail.com>
2021-03-17 09:20:42 +0700

    redux

---
+++ week6/jobsheet6/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Provider } from "react-redux";
+import { createStore, compose } from "redux";
+import MainReducer from "./reducers/MainReducer";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Table from "./containers/Table";
+import CreateTodo from "./containers/CreateTodo";
+
+const store = compose(
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+)(createStore)(MainReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CreateTodo />
+    <Table />
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
